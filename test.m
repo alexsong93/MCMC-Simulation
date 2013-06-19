e0543e78a4ca9d6b518dbfeeddc78947434ec437
Alex Song <ss456@duke.edu>
2013-06-19 15:40:17 -0400

    del

--- test.m
+++
@@ -1,9 +0,0 @@
-clc;clear;
-x=round(5*rand([1,1000])+1);
-[si sj] = ndgrid(1:6);
-s2 = [si(:) sj(:)]; % combinations for 2 contiguous states
-tm2 = zeros([numel(si),6]); % initialize transition matrix
-for i = 3:numel(x) % construct transition matrix
-    tm2(strmatch(num2str(x(i-2:i-1)),num2str(s2)),x(i))=...
-    tm2(strmatch(num2str(x(i-2:i-1)),num2str(s2)),x(i))+1
-end
